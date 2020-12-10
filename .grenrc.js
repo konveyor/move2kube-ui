b6af66fea3de4a9b2e782e36b383a31d4e3d99ba
Harikrishnan Balagopal <harikrishmenon@gmail.com>
2020-12-10 21:01:35 +0530

    fix: release workflow to match the cli release workflow
    Signed-off-by: Harikrishnan Balagopal <harikrishmenon@gmail.com>

---
+++ .grenrc.js
@@ -0,0 +1,27 @@
+let printPreamble = true;
+const preamble = `For more documentation and support please visit https://konveyor.io/move2kube/
+# Changelog
+`;
+
+function printPreambleAndGroupName({ heading }) {
+    const line = `\n## ${heading}\n`;
+    if (printPreamble) {
+        printPreamble = false;
+        return preamble + line;
+    }
+    return line;
+}
+
+module.exports = {
+    "dataSource": "prs",
+    "prefix": "Move2Kube UI ",
+    "groupBy":
+    {
+        "🚀 Features": ["enhancement"],
+        "🐛 Bug Fixes": ["bug"]
+    },
+    "template": {
+        "group": printPreambleAndGroupName,
+        "issue": ({ name, text, url }) => `- ${name} [${text}](${url})`,
+    }
+}
